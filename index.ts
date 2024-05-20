5e69462d6de06de7a2dfe80696b2ff1f305d51cb
Mirza Nauman Baig <Mirza.Baig@oma.org>
2024-05-20 20:42:17 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+import inquirer from "inquirer";
+ 
+const answer = await inquirer.prompt([
+    {
+        name:"Sentence",
+        type:"input",
+        message:"Enter you sentence to count the words : "
+    }
+]) ;
+
+const words = answer.Sentence.trim().split(" ");
+console.log("Your sentence word count is : "+words.length );
